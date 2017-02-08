3ba1a8d1671dd4da8b8d173a32694bfdae340811
Andrei Tatar <tatarandrei@gmail.com>
2017-02-08 19:45:26 +0200

    Add project files.

---
+++ SensorsNetwork/SensorsNetwork.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SensorsNetwork.ino
+ Created:	2/8/2017 7:45:22 PM
+ Author:	andrei
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
