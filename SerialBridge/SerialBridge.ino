983e4602464bc337253f4be03714330051fca75b
Andrei Tatar <tatarandrei@gmail.com>
2017-05-24 23:01:47 +0300

    update serial bridge to do the message decryption

--- SerialBridge/SerialBridge.ino
+++ SerialBridge/SerialBridge.ino
@@ -1,9 +1,14 @@
 #include <RF24.h>
 #include <nRF24L01.h>
+#include <AESLib.h>
+
+#define MAX_KEYS    10
 
 RF24 radio(2, 3);
 static bool radioConfigured = false;
-
+static uint8_t keys[MAX_KEYS][16];
+static uint8_t key_count;
+const uint8_t iv[16];
 void sendPacket(const uint8_t *data, uint8_t size);
 
 #define FRAME_CONFIGURE     0x90
@@ -18,11 +23,28 @@ void setup() {
 void loop() {
     updatePacket();
     while (radioConfigured && radio.available()) {
-        static uint8_t rxBuffer[33];
+        static uint8_t rxBuffer[34];
         uint8_t length = radio.getDynamicPayloadSize();
-        radio.read(&rxBuffer[1], length);
-        rxBuffer[0] = FRAME_RECEIVEPACKET;
-        sendPacket(rxBuffer, length + 1);
+        radio.read(&rxBuffer[2], length);
+        
+        for (uint8_t keyIndex = 0; keyIndex < key_count; keyIndex++) {
+            uint8_t decrypted[32];
+            memcpy(decrypted, &rxBuffer[2], length);
+            aes128_cbc_dec(keys[keyIndex], iv, decrypted, length);
+            uint8_t dataLength = decrypted[2];
+            if (dataLength > 29) continue;
+
+            uint16_t chksum = getChecksum(&decrypted[2], length - 2);
+            if ((chksum >> 8) == decrypted[0] && 
+                (chksum & 0xFF) == decrypted[1]) {
+                rxBuffer[0] = FRAME_RECEIVEPACKET;
+                rxBuffer[1] = keyIndex;
+                
+                memcpy(&rxBuffer[2], &decrypted[3], dataLength);
+                sendPacket(rxBuffer, dataLength + 2);
+                break;
+            }
+        }
     }
 }
 
@@ -30,22 +52,29 @@ void onPacketReceived(uint8_t *data, uint8_t size) {
     switch (*data)
     {
     case FRAME_CONFIGURE:
-        if (!radio.begin()) return;
+        if (!radio.begin()) break;
         radio.setChannel(data[6]);
         radio.setAutoAck(false);
         radio.enableDynamicPayloads();
         radio.openReadingPipe(1, &data[1]);
         radio.startListening();
         radioConfigured = true;
+        if (data[7] > MAX_KEYS) break;
+        key_count = data[7];
+        memcpy(keys, &data[8], key_count * 16);
 
         data[0] = FRAME_CONFIGURED;
         sendPacket(data, 1);
         break;
 
     case FRAME_SENDPACKET:
-        radio.openWritingPipe(&data[1]);
+        if (!radioConfigured) break;
+        radio.openWritingPipe(&data[2]);
         radio.stopListening();
-        radio.write(&data[6], size - 6);
+        
+        size = size - 7 <= 16 ? 16 : 32;
+        aes128_cbc_enc(keys[data[1]], iv, &data[7], size);
+        radio.write(&data[7], size);
         radio.startListening();
         break;
 
@@ -81,6 +110,14 @@ void updateChecksum(uint16_t *checksum, uint8_t data) {
     *checksum ^= data;
 }
 
+static uint16_t getChecksum(uint8_t *data, uint8_t length) {
+    uint16_t checksum = CHKSUM_INIT;
+    while (length--) {
+        updateChecksum(&checksum, *data++);
+    }
+    return checksum;
+}
+
 #define RX_STATUS_IDLE  0
 #define RX_STATUS_HDR   1
 #define RX_STATUS_SIZE  2
@@ -90,7 +127,7 @@ void updateChecksum(uint16_t *checksum, uint8_t data) {
 
 void updatePacket() {
     static uint8_t status = RX_STATUS_IDLE;
-    static uint8_t rxBuffer[64];
+    static uint8_t rxBuffer[200];
     static uint8_t rxSize, rxOffset;
     static uint16_t chksum, rxChksum;
     int data;
