3a39adb2c623944c29a6e9855149bc130627f59b
Andrei Tatar <tatarandrei@gmail.com>
2017-09-12 15:00:49 +0300

    set baud

--- TelnetBridge/communication/telnet.ts
+++ TelnetBridge/communication/telnet.ts
@@ -5,8 +5,9 @@ import * as net from 'net';
 import * as _ from 'lodash';
 
 export class Telnet implements ConnectableMessageLayer {
-    private socket: net.Socket;
     private readonly _data: Subject<Buffer>;
+    private socket: net.Socket;
+    private reconnectTimeout: NodeJS.Timer;
 
     constructor(private host: string, private port: number) {
         this._data = new Subject<Buffer>();
@@ -21,28 +22,33 @@ export class Telnet implements ConnectableMessageLayer {
 
             this.socket = new net.Socket();
             this.socket.on('data', data => this._data.next(data));
-            this.socket.on('disconnect', () => {
+            this.socket.once('disconnect', () => {
                 logger.warn('telnet: disconnected from server');
-                setTimeout(() => this.open(), 5000);
+                this.reconnectTimeout = setTimeout(() => this.open(), 5000);
             });
             this.socket.once('error', err => {
                 logger.warn('telnet: error: ', err.message);
-                setTimeout(() => this.open(), 5000);
+                this.reconnectTimeout = setTimeout(() => this.open(), 5000);
             });
 
             logger.debug('telnet: connecting');
-            this.socket.connect(this.port, this.host, () => {
+            this.socket.connect(this.port, this.host, async () => {
                 logger.debug('telnet: connected');
+                await this.setBaud(57600);
                 resolve();
             });
         });
     }
 
-    close(): Promise<void> {
-        throw new Error("Method not implemented.");
+    async close() {
+        clearTimeout(this.reconnectTimeout);
+        if (this.socket) {
+            this.socket.end();
+            this.socket = null;
+        }
     }
 
-    send(data: Buffer) {
+    async send(data: Buffer) {
         if (!this.socket) throw new Error('Layer not initialized');
 
         const bytesToEscape = _.countBy(data, byte => byte === 0xFF).true || 0;
@@ -54,10 +60,33 @@ export class Telnet implements ConnectableMessageLayer {
                 offset = escapedData.writeUInt8(byte, offset);
             }
         }
-        return new Promise<void>(resolve => this.socket.write(escapedData, resolve));
+
+        await this.sendRaw(escapedData);
     }
 
     get data() {
         return this._data.asObservable();
+    }
+
+    private sendRaw(data: Buffer) {
+        return new Promise<void>(resolve => this.socket.write(data, resolve));
+    }
+
+    private async setBaud(baud: number) {
+        let changeBaud = Buffer.from([
+            0xFF, //IAC
+            0xFA, //command sequence begin
+            0x2C, //port options
+            0x01, //set baud
+            0x00, //baud b1
+            0x00, //baud b2
+            0x00, //baud b3
+            0x00, //baud b4
+            0xFF, //IAC
+            0xF0, //command sequence end
+        ]);
+        changeBaud.writeUInt32BE(baud, 4);
+        console.log(changeBaud);
+        await this.sendRaw(changeBaud);
     }
 }
---
+++ TelnetBridge/sensors/dimmer.ts
@@ -0,0 +1,32 @@
+import { MessageLayer } from './../communication/interfaces';
+
+export class Dimmer {
+    private _brightness: number;
+
+    private static readonly cmd_SetBrightness = 0x01;
+    private static readonly cmd_GetStatus = 0x02;
+    private static readonly cmd_SetLedBrightness = 0x03;
+
+    private static readonly rsp_BrightnessLevel = 0x02;
+
+    constructor(private layer: MessageLayer) {
+    }
+
+    protected async init() {
+        await this.layer.send(Buffer.from([0x02]));
+    }
+
+    protected onMessage(msg: Buffer) {
+        switch (msg[0]) {
+            case Dimmer.rsp_BrightnessLevel: this._brightness = msg[1];
+        }
+    }
+
+    async setBrightness(value: number) {
+        await this.layer.send(Buffer.from([Dimmer.cmd_SetBrightness, value]));
+    }
+
+    async setLedBrightness(value: number) {
+        await this.layer.send(Buffer.from([Dimmer.cmd_SetLedBrightness, value]));
+    }
+}
