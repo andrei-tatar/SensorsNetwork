4ae5bb0bf806701094a5e75bacde31de65a6a8fd
Andrei Tatar <tatarandrei@gmail.com>
2017-09-14 22:03:11 +0300

    remove test key

--- InputSensor/InputSensor.ino
+++ InputSensor/InputSensor.ino
@@ -2,8 +2,7 @@
 
 #define PIN_INPUT   2
 
-uint8_t key[] = { 0xae, 0xbb, 0xf1, 0xa9, 0xbe, 0x6d, 0x9c, 0x24, 0xbc, 0xdc, 0x59, 0x3a, 0x55, 0x29, 0xa3, 0x8c };
-//uint8_t key[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+uint8_t key[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 Sensor sensor(key, 3, 4);
 
 void setup() {
