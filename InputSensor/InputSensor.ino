ccce0cabf27eb6dabd4aadb21469af5c523ee4d9
Andrei Tatar <tatarandrei@gmail.com>
2017-09-14 22:05:24 +0300

    remove pullup

--- InputSensor/InputSensor.ino
+++ InputSensor/InputSensor.ino
@@ -1,14 +1,15 @@
 #include "SensorLib.h"
 
-#define PIN_INPUT   2
+#define PIN_INPUT       2
+#define PIN_INTERRUPT   0
 
 uint8_t key[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 Sensor sensor(key, 3, 4);
 
 void setup() {
     sensor.begin();
-    pinMode(PIN_INPUT, INPUT_PULLUP);
-    attachInterrupt(0, inputStateChanged, CHANGE);
+    pinMode(PIN_INPUT, INPUT);
+    attachInterrupt(PIN_INTERRUPT, inputStateChanged, CHANGE);
 }
 
 void inputStateChanged() {
