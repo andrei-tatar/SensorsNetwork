70bfa0d3751b3b713fbb0124f7c8f728ff02e6cb
Andrei Tatar <tatarandrei@gmail.com>
2018-01-24 22:19:36 +0200

    use max44009 for light and bme280 for temp,pressure,humidity

--- InputSensor/InputSensor.ino
+++ InputSensor/InputSensor.ino
@@ -1,5 +1,6 @@
+#include <Adafruit_BME280.h>
 #include "SensorLib.h"
-#include "Tcn.h"
+#include "MAX44009.h"
 
 #define PIN_INPUT       2
 #define PIN_INTERRUPT   0
@@ -8,15 +9,29 @@
 
 uint8_t key[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 Sensor sensor(key, 3, 4);
-Tcn temp;
+Adafruit_BME280 bme;
+MAX44009 max44009;
 
 void setup() {
+    if (!bme.begin(0x76) || !max44009.initialize()) {
+        while (1) {
+            sensor.powerDown(10000);
+        }
+    }
+
+    bme.setSampling(
+        Adafruit_BME280::sensor_mode::MODE_NORMAL, 
+        Adafruit_BME280::sensor_sampling::SAMPLING_X16, 
+        Adafruit_BME280::sensor_sampling::SAMPLING_X16, 
+        Adafruit_BME280::sensor_sampling::SAMPLING_X16, 
+        Adafruit_BME280::sensor_filter::FILTER_X4, 
+        Adafruit_BME280::standby_duration::STANDBY_MS_1000);
+
     sensor.begin();
     pinMode(PIN_INPUT, INPUT);
     attachInterrupt(PIN_INTERRUPT, inputStateChanged, CHANGE);
     pinMode(PIN_LIGHT_POWER, OUTPUT);
     digitalWrite(PIN_LIGHT_POWER, LOW);
-    temp.begin();
 }
 
 void inputStateChanged() {
@@ -25,21 +40,29 @@ void inputStateChanged() {
 }
 
 void loop() {
-    uint8_t msg[10] = { 'B', 0, 'T', 0, 0, 'L', 0, 0, 'S', digitalRead(PIN_INPUT) };
+    uint8_t msg[16] = { 'B', 0, 'T', 0, 0, 'L', 0, 0, 'S', 0, 'H', 0, 0, 'P', 0, 0 };
     uint16_t voltage = sensor.readVoltage();
     msg[1] = voltage / 10 - 100;
 
-    uint16_t temperature = temp.read();
+    uint16_t temperature = bme.readTemperature() * 256;
     msg[3] = temperature >> 8;
     msg[4] = temperature;
 
-    digitalWrite(PIN_LIGHT_POWER, HIGH);
-    uint16_t light = analogRead(PIN_LIGHT);
-    digitalWrite(PIN_LIGHT_POWER, LOW);
-    uint16_t resistor = 100UL * 1023 / light - 100; //light resistor = 10kohm*(1023/read_value-1) kohm
-    msg[6] = resistor >> 8;
-    msg[7] = resistor;
+    uint16_t humidity = bme.readHumidity() * 100;
+    msg[11] = humidity >> 8;
+    msg[12] = humidity;
+
+    uint16_t pressure = bme.readPressure() / 10;
+    msg[14] = pressure >> 8;
+    msg[15] = pressure;
+
+    uint16_t light = max44009.getMeasurement();
+    msg[6] = light >> 8;
+    msg[7] = light;
+
+    uint8_t state = digitalRead(PIN_INPUT);
+    msg[9] = state;
 
     sensor.sendAndWait(msg, sizeof(msg));
-    sensor.powerDown(600);
+    sensor.powerDown(state ? 10 : 300);
 }
